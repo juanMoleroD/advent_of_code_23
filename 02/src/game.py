b745d20716098bd1f5da48f302a8e092a7f3ae56
juanMoleroD <juanldimeco@gmail.com>
2023-12-03 15:24:12 +0000

    added file reading@ ' :

--- 02/src/game.py
+++ 02/src/game.py
@@ -1,36 +1,29 @@
-class game():
-    def __init__(self, id:int, rounds: []):
-        self.id:int = id
-        self.rounds = rounds
-
-def gameFactory(input: str) -> game:
-    parsedId: int = int(input[5])
-    parsedRounds = []
-    result = game(parsedId, parsedRounds)
-    return result
-
-
 def checkCubes(numOfReds: int, numOfGreens: int, numOfBlues: int, input: str) -> bool:
-    removeGameId = input.split(':')[1]
-    rounds = removeGameId.split(";")
+    game = input.split(':')
+    rounds = game[1].split(";")
     inputs = []
     for round in rounds:
         inputs.append(round.split(','))
     # print(inputs)
-
     for round in inputs:
         for input in round:
             if input[-1] == 'd': # as in reD
                 num = input.split(' ')[1]
                 if int(num) > numOfReds: 
-                    return False
+                    return 0
             if input[-1] == 'n': # as in greeN
                 num = input.split(' ')[1]
                 if int(num) > numOfGreens: 
-                    return False
+                    return 0
             if input[-1] == 'e': # as in bluE
                 num = input.split(' ')[1]
                 # print(f'if num {num} > {numOfReds} return false')
                 if int(num) > numOfBlues: 
-                    return False
-    return True
+                    return 0
+    return int(game[0].split(" ")[1])
+
+def checkGameFile(file, numOfRGBCubes: []) -> int:
+    sum: int = 0
+    for line in file:
+        sum += checkCubes(numOfRGBCubes[0], numOfRGBCubes[1], numOfRGBCubes[2], line)
+    return sum
