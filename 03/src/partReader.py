5479df6925c90b54e6b90401cbb8267e0c5ddcab
juanMoleroD <juanldimeco@gmail.com>
2023-12-04 15:53:12 +0000

    added check for sides

--- 03/src/partReader.py
+++ 03/src/partReader.py
@@ -1,4 +1,4 @@
-def findPart(lines: []) -> {}:
+def findPotentialPart(lines: []) -> {}:
     partX: int = -1
     partY: int = -1
     partLen: int = -1
@@ -17,3 +17,18 @@ def findPart(lines: []) -> {}:
     value: int = int(lines[partY][partX:(partX + partLen)])
     return { "partX": partX, "partY": partY, "partLen": partLen , "value": value}
 
+def checkIfPart(potentialPart: {}, lines: []):
+    # check sides
+    sides = []
+    leftChar: str = lines[potentialPart["partY"]][potentialPart["partX"] - 1]
+    rightChar: str = lines[potentialPart["partY"]][potentialPart["partX"] + potentialPart["partLen"]]
+    sides.append(leftChar)
+    sides.append(rightChar)
+
+    for side in sides:
+        if checkIfPartSymbol(side):
+            return True
+    return False
+
+def checkIfPartSymbol(char: str) -> bool:
+    return char != '.' and char.isalpha() == False
--- 03/tests/test_partReader.py
+++ 03/tests/test_partReader.py
@@ -3,13 +3,25 @@ from src.partReader import *
 
 class Test_partReader(unittest.TestCase):
     
-    def test_findpart(self):
-        input: [] = ['..........',
-                     '...1111...',
-                     '..........']
-        expected = { "value": 1111, "partX": 3, "partY": 1, "partLen": 4 }
-        self.assertEqual(findPart(input), expected)
+    def setUp(self):
+        self.input1: [] = [ '..........',
+                            '...1111...',
+                            '..........']
+        self.input2: [] = [ '..........',
+                            '...1111...',
+                            '..........']
 
+    def test_findPotentialPart(self):
+        expected = { "value": 1111, "partX": 3, "partY": 1, "partLen": 4 }
+        self.assertEqual(findPotentialPart(self.input1), expected)
+
+    def test_checkIfPart(self):
+        potentialPart = findPotentialPart(self.input1)
+        self.assertEqual(checkIfPart(potentialPart, self.input1), False)
+        self.input1[1] = "..+1111..."
+        self.assertEqual(checkIfPart(potentialPart, self.input1), True)
+        self.input1[1] = "...1111-.."
+        self.assertEqual(checkIfPart(potentialPart, self.input1), True)
 
 
     def test_parseInputToMatrix(self):
